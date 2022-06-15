f945dfce14a9b33bf8936f1ce35d5e587fcb12b6
Shwetakotak <shwetakotak1@gmail.com>
2022-06-15 08:24:43 -0400

    Added h1 and footer to App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,28 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        {" "}
+        This project was coded by Shweta and is{" "}
+        <a
+          href="https://github.com/Shwetakotak/week5_reactapp"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          open-sourced on Github
+        </a>{" "}
+        and{" "}
+        <a
+          href="https://62a88d1160770c29af5a4bbf--eloquent-cactus-5c8803.netlify.app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          hosted on Netlify
+        </a>
+      </footer>
+    </div>
+  );
 }
