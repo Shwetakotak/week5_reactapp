cfe011b066387b914bf058fb1dcedcde96c2aa2c
Shwetakotak <shwetakotak1@gmail.com>
2022-06-23 00:23:35 -0400

    Added WeatherForecast Day1

--- src/WeatherForcast.js
+++ src/WeatherForcast.js
@@ -1,34 +1,30 @@
 import React, { useState } from "react";
-import WeatherIcon from "./WeatherIcon";
 import "./WeatherForcast.css";
 import axios from "axios";
+import WeatherForcastDay from "./WeatherForcastDay";
 
 export default function WeatherForcast(props) {
   let [loaded, setLoaded] = useState(false);
   let [forecast, setForecast] = useState(null);
+
   function handleResponse(response) {
     setForecast(response.data.daily);
     setLoaded(true);
   }
-  console.log(props);
+
   if (loaded) {
     console.log(forecast);
     return (
       <div className="weatherForcast">
         <div className="row">
           <div className="col">
-            <div className="weatherForcast-day">Tue</div>{" "}
-            <WeatherIcon code="01d" size={36} />
-            <div className="weatherForcast-temperatures">
-              <span className="weatherForcast-temperature-max">33°</span>
-              <span className="weatherForcast-temperature-min">18°</span>
-            </div>
+            <WeatherForcastDay data={forecast[0]} />
           </div>
         </div>
       </div>
     );
   } else {
-    let apiKey = "861fb20b505f7e96a549db90c19a5142";
+    let apiKey = "401ea295cd15ca3931349033bea1a34c";
     let longitude = props.coordinates.lon;
     let latitude = props.coordinates.lat;
     let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
